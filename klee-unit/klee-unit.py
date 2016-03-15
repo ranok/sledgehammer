ec183570552fe6487f8766fabab5d1e578bd459b
Jacob Torrey <jacob@jacobtorrey.com>
2016-03-15 10:54:12 +0000

    Scaffolding of klee-unit
    Signed-off-by: Jacob Torrey <jacob@jacobtorrey.com>

---
+++ klee-unit/klee-unit.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+from pycparser import c_parser, c_ast
+from ctags import CTags, TagEntry
+
+def collect_klee_runs():
+    '''Navigates all KLEE output directories and performs basic triage'''
+
+def run_klee(filename):
+    '''Runs KLEE on a given file'''
+    return subprocess.call(['klee', '--libc=uclibc', '--posix-runtime', filename])
+
+def compile(filename, outname = 'kleeunit.bc'):
+    '''Compiles for execution with KLEE'''
+    return subprocess.call(['clang', '-g', '--emit-llvm', '-c', filename, '-o', outname])
+
+def run_ctags(filename):
+    '''Executes the ctags command on the passed filename to generate the tags file'''
+    return subprocess.call(['ctags', filename])
+
+def parse_pattern(pattern):
+    '''Parses a ctags pattern string'''
+    pattern = pattern[2:-2] + ";"
+    parser = c_parser.CParser()
+    try:
+        node = parser.parse(pattern, filename = '<stdin>')
+    except c_parser.ParseError:
+        print "Unable to parse pattern: " + pattern
+        sys.exit(-1)
+    return (node.ext[-1].name, _explain_type(node.ext[-1])) 
+    
+def _explain_type(decl):
+    '''Recursively explains a type decl node'''
+    typ = type(decl)
+    
+    if typ == c_ast.TypeDecl:
+        quals = ' '.join(decl.quals) + ' ' if decl.quals else ''
+        return quals + _explain_type(decl.type)
+    elif typ == c_ast.Typename or typ == c_ast.Decl:
+        return _explain_type(decl.type)
+    elif typ == c_ast.IdentifierType:
+        return ' '.join(decl.names)
+    elif typ == c_ast.PtrDecl:
+        quals = ' '.join(decl.quals) + ' ' if decl.quals else ''
+        return quals + _explain_type(decl.type) + "*"
+    elif typ == c_ast.ArrayDecl:
+        arr = 'array'
+        if decl.dim: arr += '[%s]' % decl.dim.value
+        
+        return arr + " of " + _explain_type(decl.type)
+    
+    elif typ == c_ast.FuncDecl:
+        if decl.args:
+            params = [_explain_type(param) for param in decl.args.params]
+        else:
+            params = []
+        return [_explain_type(decl.type), params]
+
+def parse_ctags():
+    '''Returns a list of all the functions and their arguments found by ctags'''
+    try:
+        tf = CTags('tags')
+    except:
+        print "Unable to find tags file!"
+        sys.exit(-1)
+    entry = TagEntry()
+    l = []
+    if 0 == tf.first(entry):
+        return []
+    while True:
+        l.append(parse_pattern(entry['pattern']))
+        if 0 == tf.next(entry):
+            break
+    return l
+
+def controller():
+    '''Main handler that dispatches calls for KLEE-unit'''
+    if len(sys.argv) != 2:
+        print "KLEE-Unit: Usage: " + sys.argv[0] + " file_to_analyze.c"
+        sys.exit(-1)
+    filename = sys.argv[1]
+    run_ctags(filename)
+    print str(parse_ctags())
+
+if __name__ == "__main__":
+    controller()
